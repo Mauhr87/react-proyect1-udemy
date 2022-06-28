2cd1879c370b5af1b2654edadf22470356be35ab
Mauricio Hidalgo <mhidalgodg@gmail.com>
2022-06-27 23:47:44 -0600

    creating card component

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        
+        <div className='bg-light-green dib br3 pa3 ma2 grow'>
+            <img src='https://robohash.org/test?200x200' alt='Robots' />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Card;
